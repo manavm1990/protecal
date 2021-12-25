b39ff3a073ea1363300993826867a700fbf7299c
Manav Misra <manavm@visionwebsoft.com>
2021-12-24 18:31:40 -0600

    :sparkles: Basic  💩 working

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
+const proteinSpan = document.querySelector("#protein-count") as HTMLSpanElement;
+const caloriesSpan = document.querySelector(
+  "#calories-count"
+) as HTMLSpanElement;
+
+let nutrient = "protein";
+
 document.querySelectorAll("input").forEach((input) => {
   input.addEventListener("click", () => {
-    console.log("clicked");
+    nutrient = input.id;
+  });
+});
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const amt = (e.target as HTMLButtonElement).innerText.slice(1);
+
+    switch (nutrient) {
+      case "calories": {
+        const count = caloriesSpan.textContent;
+        caloriesSpan.textContent = (Number(count) + Number(amt)).toString();
+        break;
+      }
+
+      default: {
+        const count = proteinSpan.textContent;
+        proteinSpan.textContent = (Number(count) + Number(amt)).toString();
+      }
+    }
   });
 });
